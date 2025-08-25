016782d05e4bf849fbc53a0737e9ab598128f23e
Hazem Gouda <hazemgouda97@yahoo.com>
2025-08-25 20:24:48 +0300

    Remove old src folder

--- src/__mocks__/axios.js
+++
@@ -1,5 +0,0 @@
-// Manual axios mock for Jest. This keeps network calls out of unit tests.
-module.exports = {
-  get: jest.fn(() => Promise.resolve({ data: {} })),
-  create: jest.fn(() => module.exports),
-};
--- src/setupTests.js
+++
@@ -1,4 +0,0 @@
-// This file is automatically picked up by react-scripts (CRA) when running tests
-// Add jest-dom matchers
-import '@testing-library/jest-dom';
-
